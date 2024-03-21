0d75cc28b6dedbe8b0560721bdf42cbed3dd077d
Pooja Sagar Ambi <pooja.a@thinkbridge.in>
2024-03-21 10:51:16 +0530

    filter code add recomnedation code add all console error remove

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,30 +3,59 @@
     return(
 
         <>
-          <div class="col-md-2 filter-s ">
-              <div className='shadow filter-bg'>
-                <form>
-                    <div class="form-group ">
-                        <h6 for="distanceFilter">Filter</h6>
-                        <label for="priceFilter">Set Distance</label>
+         <div className="col-md-2 filter-s ">
+                <div className='shadow filter-bg'>
+                    <form>
+                        <div className="form-group mt-1">
+                            <h6 htmlFor="distanceFilter">Filter</h6>
+                            <label htmlFor="priceFilter">Set Distance</label>
 
-                        <select class="form-control rounded-pill" id="distanceFilter">
-                            <option value="">All</option>
-                            <option value="5 miles">5 miles</option>
-                            <option value="10 miles">10 miles</option>
-                            
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="priceFilter">{brand} Set Price</label>
-                        <select class="form-control rounded-pill" id="priceFilter">
-                            <option value="">All</option>
-                            <option value="$10.99">$10.99</option>
-                            <option value="$19.99">$19.99</option>
-                       
-                        </select>
-                    </div>
-                </form>
+                            <select className="form-control rounded-pill mt-1" id="distanceFilter ">
+                                <option value="">All</option>
+                                <option value="5 miles">5 miles</option>
+                                <option value="10 miles">10 miles</option>
+
+                            </select>
+                        </div>
+                        <div className="form-group mt-1">
+                            <label htmlFor="priceFilter">{brand} Set Price</label>
+                            <select className="form-control rounded-pill mt-1" id="priceFilter">
+                                <option value="">All</option>
+                                <option value="$10.99">$10.99</option>
+                                <option value="$19.99">$19.99</option>
+
+                            </select>
+                        </div>
+                        <div className="form-group mt-1">
+                            <label htmlFor="categoryFilter">Category</label>
+                            <select className="form-control rounded-pill mt-1" id="categoryFilter">
+                                <option value="">All</option>
+                                <option value="Womens">Womens</option>
+                                <option value="Mens">Mens</option>
+                                <option value="Kids">Kids</option>
+                                <option value="Kitchen">Kitchen</option>
+                                <option value="Music">Music</option>
+                            </select>
+                        </div>
+                        <div className="form-group mt-1">
+                            <label htmlFor="accesFilter">Accessories</label>
+                            <select className="form-control rounded-pill mt-1" id="accesFilter">
+                                <option value="">All</option>
+                                <option value="Mens">Mens</option>
+                                <option value="Womens">Womens</option>
+                                <option value="Kids">Kids</option>
+                            </select>
+                        </div>
+                        <div className="form-group mt-2">
+                            Todays deals
+                        </div>
+                        <div className="form-group mt-1">
+                            Offers
+                        </div>
+                        <div className="form-group mt-1">
+                            Trending
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,6 +4,80 @@ import { BsTrash3 } from 'react-icons/bs';
 import { AiOutlineHeart } from 'react-icons/ai';
 
 const Cart = () => {
+  const products = [
+    {
+      id: 1,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Organic Shampoo",
+      description:
+        "Seller name",
+      size: "Xl, Sm, M, L",
+      price: "₹299",
+    },
+    {
+      id: 2,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Luxury Handmade Soap",
+      description:
+        "Pamper your skin with our  ",
+      size: "Xl, Sm, M, L",
+      price: "$34.67",
+    },
+    {
+      id: 2,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Luxury Handmade Soap",
+      description:
+        "Pamper your skin with our  ",
+      size: "Xl, Sm, M, L",
+      price: "$34.67",
+    },
+    {
+      id: 3,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Anti-Aging Face Cream",
+      description:
+        "Pamper your skin with our ",
+      size: "Xl, Sm, M, L",
+      price: "$28.67",
+    },
+    {
+      id: 4,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Exfoliating Body Scrub",
+      description:
+        "Pamper your skin with our ",
+      size: "Xl, Sm, M, L",
+      price: "$24.67",
+    },
+    {
+      id: 5,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Hydrating Lip Balm",
+      description:
+        "Pamper your skin with our ",
+      size: "Xl, Sm, M, L",
+      price: "$21.27",
+    },
+    {
+      id: 5,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Hydrating Lip Balm",
+      description:
+        "Pamper your skin with our ",
+      size: "Xl, Sm, M, L",
+      price: "$21.27",
+    },
+    {
+      id: 5,
+      img: "http://localhost:5173/src/components/images/product.png",
+      name: "Hydrating Lip Balm",
+      description:
+        "Pamper your skin with our ",
+      size: "Xl, Sm, M, L",
+      price: "$21.27",
+    },
+  ];
 
     return (
 
@@ -199,6 +273,36 @@ const Cart = () => {
                 </Link >
               </div>
             </div>
+              {/* div for Recomended */}
+              <div className='card'>
+                <div className="card-header py-2 rounded-pill">
+                  <h5 className="mb-0">Recommended Items</h5>
+                </div>
+                <div className="d-flex gap-3   overflow-x-auto my-3">
+                  {products.map((prod,index) => (
+                    <div key={index} className="d-flex  flex-column bg-light  shadow rounded px-4" style={{ height: '430px' }}  >
+                      <img className="w-100 h-50 rounded" src={prod.img} alt={`Image ${prod.id}`} />
+                      <div className="d-flex flex-column justify-content-between p-1">
+                        <div className="d-flex flex-column">
+                          <h1 className="fs-4">{prod.name}</h1>
+                          <p className="text-muted">{prod.description}</p>
+                          <p className="text-muted">{prod.size}</p>
+                        </div>
+                        <div className="d-flex justify-content-center align-items-center fs-4 my-1 pb-1">
+                          <button className="btn  btn-dark w-100">❤</button>
+                          <div className="d-flex ">
+                            <button className="btn btn-dark w-100 mx-2 px-5">Buy</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+
+
+
+                </div>
+              </div>
+
           </div>
         </div>
       </div>
