759b7552abb0439ef74873f0cca42526218c7db1
Shubham Laware <lawareshubham647@gmail.com>
2024-04-09 16:51:28 +0530

    Shubham- Ads carosel

--- src/components/AdsCarousel.jsx
+++ src/components/AdsCarousel.jsx
@@ -1,35 +1,192 @@
-import React, { useState } from 'react';
+  import React, { useEffect, useState } from "react";
+  import StarRatings from "./ProductInfo/StarRatings";
+  import cartIcon from "../assets/cart-icon.svg";
+  import { Link } from "react-router-dom";
 
-function AdsCarousel({ productImages }){
-  const [currentSlide, setCurrentSlide] = useState(0);
+  import {
+    addToCart,
+    showSnackbar,
+    hideSnackbar,
+    addItemToWishlist,
+    hideSnackbarForWishlist,
+    showSnackbarForWishlist,
+  } from "../components/redux/Slices/CartSlice.js";
+  import { useDispatch, useSelector } from "react-redux";
+  import ProductCard from "./ProductInfo/ProductCard";
 
-  const goToPrevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? productImages.length - 1 : currentSlide - 1);
-  };
+  function AdsCarousel({ products }) {
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart);
+    const handleAddToCart = (product, index) => {
+      dispatch(addToCart(product));
+      dispatch(showSnackbar({ message: "Product added successfully!", index }));
 
-  const goToNextSlide = () => {
-    setCurrentSlide(currentSlide === productImages.length - 1 ? 0 : currentSlide + 1);
-  };
+      // Wait for 1 second, then hide snackbar
+      setTimeout(() => {
+        dispatch(hideSnackbar());
+      }, 1000);
+    };
 
-  return (
-    <div className="vertical carousel slide" data-bs-ride="carousel" style={{ height: '100%', overflow: 'hidden' }}>
-      <div className="carousel-inner h-100" style={{ display: 'flex', flexDirection: 'column' }}>
-        {productImages.map((image, index) => (
-          <div key={index} className={`h-100 carousel-item ${index === currentSlide ? 'active' : ''}`} style={{ transform: 'rotate(90deg)' }}>
-            <img className="d-block w-100 h-100" src={image} alt={`Slide ${index + 1}`} style={{ transform: 'rotate(-90deg)' }} />
-          </div>
-        ))}
+    useEffect(() => {
+      setCurrentSlide(0); // Reset current slide to the first one
+    }, [products]);
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const goToPrevSlide = () => {
+      setCurrentSlide(
+        currentSlide === 0 ? products.length - 1 : currentSlide - 1
+      );
+    };
+
+    const goToNextSlide = () => {
+      setCurrentSlide(
+        currentSlide === products.length - 1 ? 0 : currentSlide + 1
+      );
+    };
+
+    return (
+      <>
+        <div
+        id="adsCarousel"
+        className="carousel slide v w-100    rounded-2"
+        data-bs-ride="carousel"
+        style={{ height: "100%" }}
+      >
+        <div className="carousel-inner rounded h-100">
+          {products.map((product, index) => (
+            <div
+              key={index} // Ensure each carousel item has a unique key
+              className={`h-100 carousel-item rounded ${
+                index === currentSlide ? "active" : ""
+              }`}
+            >
+               <div key={index} className=" w-100 ">
+                  <div className="product-card " style={{ height: "100%" }}>
+                    <a
+                      href={`/${product.product_id}`}
+                      target="_blank"
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                      }}
+                    >
+                      <div className="product-image">
+                        <img src={product.product_image1} alt="Product 1" />
+                        <div className="offer-tag bg-warning rounded-pill text-center p-1 text-light">
+                          {product.offers}% Off
+                        </div>
+                      </div>
+
+                      <div className="product-content">
+                        {product.product_name.length > 15
+                          ? product.product_name.substring(0, 25) + "..."
+                          : product.product_name}
+                        <h5>
+                          Price: <sup>&#x20B9;</sup>
+                          {product.product_price}
+                          <span className="text-decoration-line-through text-muted fs-6 fw-light">
+                            599
+                          </span>
+                          <span
+                            className="text-muted"
+                            style={{
+                              fontSize: "13px",
+                            }}
+                          >
+                            {" "}
+                            {product.product_stock}
+                          </span>
+                        </h5>
+                        <div className="product-rating text-warning">
+                          Rating: <StarRatings rating={product.product_ratings} />
+                        </div>
+                        <p className="product-distance text-secondary ">
+                          Distance: {product.distance}km away.
+                        </p>
+                        {cart.snackbar.open && cart.snackbar.index === index && (
+                          <div
+                            style={{ fontSize: "12px" }}
+                            className="border text-center rounded w-75 mx-auto"
+                          >
+                            {cart.snackbar.message}
+                          </div>
+                        )}
+                      </div>
+                    </a>
+
+                    <div className="d-flex justify-content-center align-items-center gap-2">
+                      <button
+                        className="btn btn-primary  ms-2"
+                        onClick={() => handleAddToCart(product, index)}
+                      >
+                        <img
+                          className="img-fluid"
+                          src={cartIcon}
+                          style={{ height: "20px" }}
+                        />
+                      </button>
+                      <button className="btn btn-primary my-2  ms-2 px-2 py-1">
+                        Buy Now
+                      </button>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev rounded-circle p-2 ms-1"
+          type="button"
+          data-bs-target="#adsCarousel" // Correct data-bs-target
+          data-bs-slide="prev"
+          style={{
+            height: "30px",
+            top: "35%",
+            width: "35px",
+            backgroundColor: "#b3b3b4",
+          }}
+        >
+          <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+        </button>
+        <button
+          className="carousel-control-next rounded-circle p-2 me-1"
+          type="button"
+          data-bs-target="#adsCarousel" // Correct data-bs-target
+          data-bs-slide="next"
+          style={{
+            height: "30px",
+            top: "35%",
+            width: "35px",
+            backgroundColor: "#b3b3b4",
+          }}
+        >
+          <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+        </button>
       </div>
-      <button className="carousel-control-prev" type="button" onClick={goToPrevSlide} style={{ bottom: 'auto', width: '30px', height: '10%',left:'40%' }}>
-        <span className="carousel-control-prev-icon" aria-hidden="true" style={{ transform: 'rotate(90deg)' }}></span>
-        <span className="sr-only">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" onClick={goToNextSlide} style={{ top: 'auto', bottom: '0', width: '30px', height: '10%',right:'40%',}}>
-        <span className="carousel-control-next-icon" aria-hidden="true" style={{ transform: 'rotate(90deg)' }}></span>
-        <span className="sr-only">Next</span>
-      </button>
-    </div>
-  );
-};
+    </>
+      
+    );
+  }
 
-export default AdsCarousel;
+  export default AdsCarousel;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -4,8 +4,17 @@ import Imgs from "../components/images/men.jpg";
 import Footer from "../components/Footer";
 import AdsCarousel from "../components/AdsCarousel";
 import { productImages } from "../components/ProductInfo/data";
+import { useContext } from "react";
+import myContext from "../components/context/MyContext";
 
 const OrdersPage = () => {
+
+  const context=useContext(myContext);
+
+  const {products}=context;
+  
+
+  
   return (
     <div className="border ">
       {/* <br></br>
@@ -14,249 +23,7 @@ const OrdersPage = () => {
       <br></br>
       <br></br> */}
 
-      {/* inside layout */}
-
-      {/* <section
-        className="h-100 gradient-custom  container  d-flex gap-5 "
-        style={{ backgroundColor: "" }}
-      >
-        <div className=" py-5 h-100 d-flex ">
-          <div className="d-flex  gap-5   align-items-center flex-wrap   ">
-            <div className="col-lg-8 col-xl-12">
-              <div className="card  " style={{ borderRadius: "10px" }}>
-                <div className="card-header px-4 py-5 col d-flex flex-column gap-2 ">
-                  <div className=" d-flex">
-                    <h5 className="text-muted mb-0  ">
-                      Thanks for your Order,{" "}
-                      <span style={{ color: "black" }}>
-                        Hemang Krishna Chaitanya
-                      </span>
-                      !
-                    </h5>
-
-                  </div>
-
-                 
-
-                </div>
-
-                <div className="mt-3 px-4">
-                    <div className="mobile-menu-logo">
-                      <div className="mobile-search " style={{padding:0}} >
-                        <form className="input-group">
-                          <input
-                            type="search"
-                            className="form-control search-box-m"
-                            style={{
-                              border: "2px solid #d4e26b",
-                              marginLeft: 0,
-                            }}
-                            placeholder="Ex: T-Shirt near me"
-                            aria-label="Search"
-                          />
-                          <button
-                            type="submit"
-                            className="btn btn-outline-success rounded"
-                          >
-                            Go
-                          </button>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="mt-3 px-4">
-                    <div className="d-flex gap-3  ">
-                      <button className="btn btn-primary ">Orders</button>
-                      <button className="btn btn-primary">Buy Again</button>
-                      <button className="btn btn-primary">
-                        Cancelled Orders
-                      </button>
-                    </div>
-                  </div>
-
-                <div className="card-body p-4">
-                  <div className="d-flex justify-content-between align-items-center mb-4">
-                    <p
-                      className="lead fw-normal mb-0"
-                      style={{ color: "#d8dfab" }}
-                    >
-                      Receipt
-                    </p>
-                    <p className="small text-muted mb-0">
-                      Receipt Voucher : 1KAU9-84UIL
-                    </p>
-                  </div>
-
-                  <div className="card shadow-0 border mb-4">
-                    <div className="card-body">
-                      <div className="row">
-                        <div className="col-md-2">
-                          <img src={Imgs} className="img-fluid" alt="Phone" />
-                        </div>
-                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
-                          <p className="text-muted mb-0">Item Name</p>
-                        </div>
-                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
-                          <p className="text-muted mb-0 small">Item clor</p>
-                        </div>
-                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
-                          <p className="text-muted mb-0 small">Item fabric</p>
-                        </div>
-                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
-                          <p className="text-muted mb-0 small">Qty: 1</p>
-                        </div>
-                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
-                          <p className="text-muted mb-0 small">599 RS</p>
-                        </div>
-                      </div>
-                      <hr
-                        className="mb-4"
-                        style={{ backgroundColor: "#e0e0e0", opacity: 1 }}
-                      />
-
-                      <div className="row">
-                        <div className="col-md-2">
-                          <p className="text-muted small">Track Order</p>
-                        </div>
-                        <div className="col-md-10">
-                          <div
-                            className="progress"
-                            style={{ height: "6px", borderRadius: "16px" }}
-                          >
-                            <div
-                              className="progress-bar"
-                              role="progressbar"
-                              style={{
-                                width: "65%",
-                                borderRadius: "16px",
-                                backgroundColor: "#E4D6D2",
-                              }}
-                            ></div>
-                          </div>
-                          <div className="d-flex justify-content-between mb-1">
-                            <p className="">Out for delivery</p>
-                            <p className="">Delivered</p>
-                          </div>
-
-                          <div className="d-flex justify-content-between pt-2">
-                            <p className="fw-bold mb-0">Order Details</p>
-                            <p className="text-muted mb-0">
-                              <span className="fw-bold me-4">Total</span>{" "}
-                              1,198.00
-                            </p>
-                          </div>
-
-                          <div className="d-flex justify-content-between pt-2">
-                            <p className="text-muted mb-0">
-                              Invoice Number : 788152
-                            </p>
-                            <p className="text-muted mb-0">
-                              <span className="fw-bold me-4">Discount</span>{" "}
-                              300.00
-                            </p>
-                          </div>
-
-                          <div className="d-flex justify-content-between">
-                            <p className="text-muted mb-0">
-                              Invoice Date : 22 Dec, 2019
-                            </p>
-                            <p className="text-muted mb-0">
-                              <span className="fw-bold me-4">GST 18%</span> 150
-                              RS
-                            </p>
-                          </div>
-
-                          <div className="d-flex justify-content-between mb-5">
-                            <p className="text-muted mb-0">
-                              Receipt Voucher : 18KU-62IIK
-                            </p>
-                            <p className="text-muted mb-0">
-                              <span className="fw-bold me-4">
-                                Delivery Charges
-                              </span>{" "}
-                              Free
-                            </p>
-                          </div>
-                        </div>
-
-                        <div className="d-flex flex-wrap align-items-center py-3">
-                          <Link
-                            to="/"
-                            className="btn btn-primary mx-1"
-                            role="button"
-                            aria-disabled=""
-                          >
-                            Track
-                          </Link>
-
-                          <Link
-                            to="/"
-                            className="btn btn-light border rounded-pill mx-1"
-                            role="button"
-                            aria-disabled=""
-                          >
-                            Feedback
-                          </Link>
-
-                          <Link
-                            to="/"
-                            className="btn btn-light border rounded-pill mx-1"
-                            role="button"
-                            aria-disabled=""
-                          >
-                            Return
-                          </Link>
-
-                          <Link
-                            to="/"
-                            className="btn btn-light border rounded-pill mx-1"
-                            role="button"
-                            aria-disabled=""
-                          >
-                            Review
-                          </Link>
-                        </div>
-
-                        <div
-                          className="card-footer border-0 px-4 py-2"
-                          style={{
-                            backgroundColor: "#E4D6D2",
-                            borderRadius: "50px",
-                          }}
-                        >
-                          <h5 className="d-flex align-items-center justify-content-end text-dark text-uppercase mb-0">
-                            Total paid:{" "}
-                            <span className="h2 mb-0 ms-2">1,048 RS</span>
-                          </h5>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            
-          </div>
-
-         
-          
-        </div>
-
-        <div className=" col-3  my-md-5  pt-md-5 " style={{ height: "350px" }}>
-              <div
-                className=" h-100  border rounded my-md-5"
-                style={{ border: "1px solid" }}
-              >
-                <h2>Ads</h2>
-              </div>
-            </div>
-      </section> */}
-
-
-
-      {/* outside layout */}
+      
 
       <div className=" mt-1  ">
       <div className=" d-flex  flex-wrap justify-content-center  ">
@@ -280,15 +47,15 @@ const OrdersPage = () => {
                   </div>
 
                   <div
-                    className="mt-4 pb-4"
+                    className="mt-4 pb-4  "
                     style={{ borderBottom: "1px solid  #c4c4c4" }}
                   >
-                    <div className="mobile-menu-logo">
-                      <div className="mobile-search " style={{ padding: 0 }}>
+                    <div className=" ">
+                      <div className=" " style={{ padding: 0 }}>
                         <form className="input-group d-flex  gap-2">
                           <input
                             type="search"
-                            className="form-control search-box-m rounded"
+                            className="form-control search-box-m rounded "
                             style={{
                               border: "2px solid #d4e26b",
                               marginLeft: 0,
@@ -675,13 +442,18 @@ const OrdersPage = () => {
             </div>
           </section>
         </div>
-        <div className="  col-12 col-md-3  px-2 px-md-5 " >
+
+
+        <div className="  col-10 col-sm-5 col-md-3  px-2 px-lg-5 " style={{marginBottom:'3.5%'}}  >
+        <div className="sticky-top" style={{ top: '82px',}}>
           <div
-            className=" border rounded mt-md-5 "
-            style={{ border: "1px solid" ,height:'380px' ,position:'relative',  }}
+            className=" border rounded mt-md-5  "
+            style={{ border: "1px solid"  ,position:'relative',  }}
           >
-            <AdsCarousel productImages={productImages}/>
+
+            <AdsCarousel products={products}/>
           </div>
+        </div>
         </div>
         </div>
       </div>
